96c06a4f66e15e4d0af26c9bd320f9e1e662ae86
SuryaStefanusSimamora <ipensimamora@gmail.com>
2024-04-03 11:39:15 +0700

    awoo

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+        public function index()
+{
+    echo "<h1> Perkenalkan</h1>";
+    echo"Nama Saya Surya Stefanus Simamora
+        Saya tinngal di daerah citeureup
+        olah raga yang saya sukai adalah 
+        Bulutangkis";
+        
+}
+}
